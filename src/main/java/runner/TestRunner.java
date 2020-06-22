51b07b1b2eb34906560dcda422954849cafe73f7
BashaLTC <janibasha@libertytestingcenter.com>
2020-06-22 22:22:30 +0530

    Signed-off-by: BashaLTC <janibasha@libertytestingcenter.com>

--- src/main/java/runner/TestRunner.java
+++ src/main/java/runner/TestRunner.java
@@ -8,7 +8,6 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.FeatureWrapper;
 import io.cucumber.testng.PickleWrapper;
